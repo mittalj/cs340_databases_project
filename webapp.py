e79b9ce0c9d467ee7b4c39259c0c09cdc50038a4
mittalj <59752236+mittalj@users.noreply.github.com>
2020-05-16 21:08:58 -0700

    Add db_connector file for database connections For a test, I added query for the qualifications table to ensure that data is getting populated on the front-end.

--- webapp.py
+++ webapp.py
@@ -1,67 +1,71 @@
 from flask import Flask, render_template
 from flask import request, redirect, url_for
+from db_connector import connect_to_database, execute_query
 #create the web application
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/members')
 def members():
-    return render_template('members.html')
+	return render_template('members.html')
 
 @app.route('/locations')
 def locations():
-    return render_template('locations.html')
+	return render_template('locations.html')
 
 @app.route('/trainers')
 def trainers():
-    return render_template('trainers.html')
+	return render_template('trainers.html')
 
 @app.route('/qualifications')
 def qualifications():
-    return render_template('qualifications.html')
+	db_connection = connect_to_database()
+	query = "SELECT qual_name, is_cert from qualifications;"
+	result = execute_query(db_connection, query).fetchall()
+	return render_template('qualifications.html',rows=result)
 
 @app.route('/programs')
 def programs():
-    return render_template('programs.html')
+	return render_template('programs.html')
 
 @app.route('/addMember')
 def addMember():
-    return render_template('addMember.html')
+	return render_template('addMember.html')
 
 @app.route('/addLocation')
 def addLocation():
-    return render_template('addLocation.html')
+	return render_template('addLocation.html')
 
 @app.route('/addTrainer')
 def addTrainer():
-    return render_template('addTrainer.html')
+	return render_template('addTrainer.html')
 
 @app.route('/addQualification')
 def addQualification():
-    return render_template('addQualification.html')
+	return render_template('addQualification.html')
 
 @app.route('/addProgram')
 def addProgram():
-    return render_template('addProgram.html')
+	return render_template('addProgram.html')
 
 @app.route('/updateMember')
 def updateMember():
-    return render_template('updateMember.html')
+	return render_template('updateMember.html')
 
 @app.route('/updateTrainer')
 def updateTrainer():
-    return render_template('updateTrainer.html')
+	return render_template('updateTrainer.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+	return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
