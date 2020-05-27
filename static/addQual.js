bce87937334abcbab2dbb068ff42538845e3ff8b
mittalj <59752236+mittalj@users.noreply.github.com>
2020-05-26 18:39:40 -0700

    .addQualButton {   margin-left:250px; }

---
+++ static/addQual.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("addMoreQual").addEventListener("click",  addQualEvent);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("removeQual").addEventListener("click",  remove_qual);
+});
+
+function addQualEvent() {
+  var newClone = document.getElementById("qualDivId").cloneNode(true);
+  var count = document.getElementsByClassName("qualDivField").length;
+  newClone.childNodes[3].id = newClone.childNodes[3].id + count
+  newClone.id = newClone.id + count
+  document.getElementById("mainQualDiv").appendChild(newClone);
+};
+
+function remove_qual()
+  {
+    var idVal = (document.getElementsByClassName("qualDivField").length) - 1;
+    if (idVal >= 1)
+    {
+      var itemToRemove = document.getElementById("qualDivId" + idVal)
+      itemToRemove.remove();
+    }
+  }
