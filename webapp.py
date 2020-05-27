9aadb6e8e2fe9ca7e4a2f1a9528f4bd6c9c48f1d
sivekumy <52305753+sivekumy@users.noreply.github.com>
2020-05-27 00:38:19 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 from flask import request, redirect, url_for, flash
 from db_connector import connect_to_database, execute_query
-from html.parser import HTMLParser
-
 #create the web application
 app = Flask(__name__)
 app.secret_key = "SECRETKEY"
@@ -29,7 +27,7 @@ def locations():
 	query = "SELECT branch_name, address_line1, address_line2, city, state, zip from locations;"
 	result = execute_query(db_connection, query).fetchall()
 	return render_template('locations.html',rows=result)
-
+	
 @app.route('/trainers')
 def trainers():
 	db_connection = connect_to_database()
@@ -56,15 +54,15 @@ def programs():
 	result = execute_query(db_connection, query).fetchall()
 	return render_template('programs.html',rows=result)
 
-@app.route('/addMember',methods=['POST','GET'])
+@app.route('/addMember', methods=['POST','GET'])
 def addMember():
 	db_connection = connect_to_database()
 	if request.method == 'GET':
-		query1 = "Select program_id, program_name from programs;"
+		query1 = "SELECT program_id, program_name from programs;"
 		result1 = execute_query(db_connection, query1).fetchall()
 		query2 = "SELECT location_id, branch_name from locations;"
 		result2 = execute_query(db_connection, query2).fetchall()
-		query3 = "SELECT trainer_id, first_name + " " + last_name from trainers;"
+		query3 = "SELECT trainer_id, first_name, last_name from trainers;"
 		result3 = execute_query(db_connection, query3).fetchall()
 		return render_template('addMember.html', programs=result1, locations=result2, trainers=result3)
 	elif request.method == 'POST':
@@ -79,12 +77,11 @@ def addMember():
 		else:
 			gender_input = 2
 		weight_input = request.form['weight']
-		program_name_input = request.form['program_name']
-		location_dropdown_input = request.form['location_id']
+		program_id_input = request.form['program_id']
+		location_dropdown_input = request.form['preferred_location']
 		trainer_id_dropdown_input = request.form['trainer_id']
-		query1 = "INSERT INTO members (first_name, last_name, birth_date, gender, weight, program_name, location_id, trainer_id) \
-		VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-		data1 = (first_name_input,last_name_input,birth_date_input,gender_input, weight_input, program_name_input, location_dropdown_input, trainer_id_dropdown_input)
+		query1 = "INSERT INTO members (first_name, last_name, birth_date, gender, weight, program_id, preferred_location, trainer_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+		data1 = (first_name_input,last_name_input,birth_date_input,gender_input, weight_input, program_id_input, location_dropdown_input, trainer_id_dropdown_input)
 		result1 = execute_query(db_connection, query1, data1)
 		myMemberID = result1.lastrowid
 		print("My Member ID:")
@@ -92,63 +89,60 @@ def addMember():
 		flash(u'A Member Has Been Added!!', 'confirmation')
 		return redirect(url_for('members'))
 
-@app.route('/addLocation',methods=['POST','GET'])
+@app.route('/addLocation', methods=['POST','GET'])
 def addLocation():
-	db_connection = connect_to_database()
 	if request.method == 'GET':
 		return render_template('addLocation.html')
 	elif request.method == 'POST':
 		branch_name_input = request.form['branch_name']
 		address_line1_input = request.form['address_line1']
 		address_line2_input = request.form['address_line2']
+		city_input = request.form['city']
 		state_input = request.form['state']
 		zip_input = request.form['zip']
-		query1 = "INSERT INTO locations(branch_name, address_line1, address_line2, state, zip) \
-		VALUES (%s,%s,%s,%s,%s)"
-		data1 = (branch_name_input, address_line1_input, address_line2_input, state_input, zip_input)
-		result1 = execute_query(db_connection, query1, data1)
-		myLocationID = result1.lastrowid
-		print("My Location ID:")
-		print(myLocationID)
+		db_connection = connect_to_database()
+		query = "INSERT INTO locations(branch_name, address_line1, address_line2, city, state, zip) VALUES (%s, %s,%s,%s,%s,%s)"
+		data = (branch_name_input, address_line1_input, address_line2_input, city_input, state_input, zip_input)
+		execute_query(db_connection, query, data)
 		flash(u'A Location Has Been Added!!', 'confirmation')
 		return redirect(url_for('locations'))
-
+		
 @app.route('/addTrainer',methods=['POST','GET'])
 def addTrainer():
-		db_connection = connect_to_database()
-		if request.method == 'GET':
-			query1 = "Select qual_id, qual_name from qualifications;"
-			result1 = execute_query(db_connection, query1).fetchall()
-			query2 = "SELECT location_id, branch_name from locations;"
-			result2 = execute_query(db_connection, query2).fetchall()
-			return render_template('addTrainer.html', quals=result1, locations=result2)
-		elif request.method == 'POST':
-			first_name_input = request.form['first_name']
-			last_name_input = request.form['last_name']
-			birth_date_input = request.form['birth_date']
-			gender_input = request.form['gender']
-			if gender_input == 'male':
-				gender_input = 0
-			elif gender_input == 'female':
-				gender_input = 1
-			else:
-				gender_input = 2
-			date_employed_input = request.form['date_employed']
-			capacity_input = request.form['capacity']
-			location_dropdown_input = request.form['location_id']
-			query1 = "INSERT INTO trainers (first_name, last_name, birth_date, gender, date_employed, capacity, location_id) \
-			VALUES (%s,%s,%s,%s,%s,%s,%s)"
-			data1 = (first_name_input,last_name_input,birth_date_input,gender_input,date_employed_input,capacity_input,location_dropdown_input)
-			result1 = execute_query(db_connection, query1, data1)
-			myTrainerID = result1.lastrowid
-			values = request.form.getlist('input_text')
-			for v in values:
-				qual_id_dropdown_input = v
-				query2="INSERT INTO trainer_qualifications (trainer_id, qual_id) VALUES (%s, %s)"
-				data2= (myTrainerID,qual_id_dropdown_input)
-				execute_query(db_connection, query2, data2)
-			flash(u'A Trainer Has Been Added!!', 'confirmation')
-			return redirect(url_for('trainers'))
+	db_connection = connect_to_database()
+	if request.method == 'GET':
+		query1 = "Select qual_id, qual_name from qualifications;"
+		result1 = execute_query(db_connection, query1).fetchall()
+		query2 = "SELECT location_id, branch_name from locations;"
+		result2 = execute_query(db_connection, query2).fetchall()
+		return render_template('addTrainer.html', quals=result1, locations=result2)
+	elif request.method == 'POST':
+		first_name_input = request.form['first_name']
+		last_name_input = request.form['last_name']
+		birth_date_input = request.form['birth_date']
+		gender_input = request.form['gender']
+		if gender_input == 'male':
+			gender_input = 0
+		elif gender_input == 'female':
+			gender_input = 1
+		else:
+			gender_input = 2
+		date_employed_input = request.form['date_employed']
+		capacity_input = request.form['capacity']
+		location_dropdown_input = request.form['location_id']
+		qual_id_dropdown_input = request.form['qual_id']
+		query1 = "INSERT INTO trainers (first_name, last_name, birth_date, gender, date_employed, capacity, location_id) \
+		VALUES (%s,%s,%s,%s,%s,%s,%s)"
+		data1 = (first_name_input,last_name_input,birth_date_input,gender_input,date_employed_input,capacity_input,location_dropdown_input)
+		result1 = execute_query(db_connection, query1, data1)
+		myTrainerID = result1.lastrowid
+		print("My Trainer ID:")
+		print(myTrainerID)
+		query2="INSERT INTO trainer_qualifications (trainer_id, qual_id) VALUES (%s, %s)"
+		data2= (myTrainerID,qual_id_dropdown_input)
+		execute_query(db_connection, query2, data2)
+		flash(u'A Trainer Has Been Added!!', 'confirmation')
+		return redirect(url_for('trainers'))
 
 @app.route('/addQualification',methods=['POST','GET'])
 def addQualification():
@@ -188,6 +182,10 @@ def updateMember():
 @app.route('/updateTrainer',methods=['POST','GET'])
 def updateTrainer():
 	return render_template('updateTrainer.html')
+
+@app.route('/updateLocation')
+def updateLocation():
+	return render_template('updateLocation.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
